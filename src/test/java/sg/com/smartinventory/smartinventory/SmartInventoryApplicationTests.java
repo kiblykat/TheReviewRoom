53fe002b377755aeea8fef43f134c575f239f86a
izzat <izzatfadzlon@gmail.com>
2024-03-18 11:26:30 +0800

    initial project setup

---
+++ src/test/java/sg/com/smartinventory/smartinventory/SmartInventoryApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.com.smartinventory.smartinventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartInventoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
