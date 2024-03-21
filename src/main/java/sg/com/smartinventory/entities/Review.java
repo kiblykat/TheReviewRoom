d828525c66d07a8e3b5da487153561ba388f802f
nhkhai@gmail.com <nhkhai@gmail.com>
2024-03-22 05:34:45 +0800

    Update Review.java
    Removed unused import.

--- src/main/java/sg/com/smartinventory/entities/Review.java
+++ src/main/java/sg/com/smartinventory/entities/Review.java
@@ -6,8 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.Min;
+
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
