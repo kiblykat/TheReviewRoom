68b1d3264c64fb4bb041fcdecb9bc05e22f4f3cf
nhkhai@gmail.com <nhkhai@gmail.com>
2024-03-22 03:36:38 +0800

    Added repository API calls.

--- src/main/java/sg/com/smartinventory/repositories/ReviewRepository.java
+++ src/main/java/sg/com/smartinventory/repositories/ReviewRepository.java
@@ -1,9 +1,33 @@
 package sg.com.smartinventory.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import sg.com.smartinventory.entities.Review;
 
+// ReviewRepository simply extends JpaRepository, giving us a lot of default methods to access the database without the hard work. 
+// This is a very powerful feature of Spring Data JPA. It will automatically do a property check and traverse the supported nested properties, translating them into the relevant queries. 
+// We can also create custom queries using the JPA criteria API by simply creating a method with a certain naming convention specified by the query creation mechanism.
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    // Custom query to find all reviews with a certain ID.
+    List<Review> findByID(long id);
 
+    // Custom query to find all reviews with a certain category.
+    List<Review> findByCategory(String category);
+
+    // Custom query to find all reviews with a certain review content.
+    List<Review> findByReviewContent(String reviewContent);
+
+    // Custom query to find all reviews with a certain rating.
+    List<Review> findByRating(int rating);
+
+    // Custom query to find all reviews with a certain customer ID.
+    List<Review> findByCustomerID(long customerId);
+
+    // Custom query to find all reviews with a certain product ID.
+    List<Review> findByProductID(long productId);
+
+    // Custom query to find all reviews with a certain customer ID and product ID.
+    List<Review> findByCustomerIDAndProductID(long customerId, long productId);
 }
