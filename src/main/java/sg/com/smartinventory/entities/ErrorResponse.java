f5ce00fd876433e969c59d405715c722b4202ae5
nhkhai <nhkhai@gmail.com>
2024-03-22 17:46:58 +0800

    Revert "Revert "Partially added JWT support.""
    This reverts commit 682f9305efbc96e2e53512c304be58335ebdd917.

---
+++ src/main/java/sg/com/smartinventory/entities/ErrorResponse.java
@@ -0,0 +1,15 @@
+package sg.com.smartinventory.entities;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private LocalDateTime timestamp;
+}
---
+++ src/test/java/sg/com/smartinventory/entities/ErrorResponseTest.java
@@ -0,0 +1,15 @@
+package sg.com.smartinventory.entities;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
+public class ErrorResponseTest {
+    @DisplayName("Error Response Test")
+    @Test
+    public void errorResponseTest() throws Exception {
+
+    }
+}
