682f9305efbc96e2e53512c304be58335ebdd917
nhkhai <nhkhai@gmail.com>
2024-03-22 17:40:12 +0800

    Revert "Partially added JWT support."
    This reverts commit 79391634cb04a5656d5b16e3be12c60d0d2b8175.

--- src/main/java/sg/com/smartinventory/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package sg.com.smartinventory.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import sg.com.smartinventory.entities.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
