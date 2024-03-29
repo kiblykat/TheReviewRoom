7b6c7d18eb93088d4767104a054578acddb922e5
ylongtay <145927097+ylongtay@users.noreply.github.com>
2024-03-29 14:46:19 +0800

    SIS-28 completed

--- src/main/java/sg/com/smartinventory/controllers/ProductController.java
+++ src/main/java/sg/com/smartinventory/controllers/ProductController.java
@@ -1,10 +1,17 @@
 package sg.com.smartinventory.controllers;
 
+import java.util.ArrayList;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
@@ -14,21 +21,68 @@ import sg.com.smartinventory.services.ProductService;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-    private ProductService productService;
+  private ProductService productService;
 
-    // @Autowired
-    public ProductController(ProductService productService) {
-        this.productService = productService;
-    }
+  // @Autowired
+  public ProductController(ProductService productService) {
+    this.productService = productService;
+  }
 
-    // CREATE.
-    @PostMapping("")
-    public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
-        // if(bindingResult.hasErrors()) {
-        // return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-        // }
+  // CREATE.
+  @PostMapping("")
+  public ResponseEntity<Product> createProduct(@Valid @RequestBody Product product) {
+    // if(bindingResult.hasErrors()) {
+    // return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    // }
 
-        Product newProduct = productService.createProduct(product);
-        return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
-    }
+    Product newProduct = productService.createProduct(product);
+    return new ResponseEntity<>(newProduct, HttpStatus.CREATED);
+  }
+
+  // Get All
+  @GetMapping({ "", "/" })
+  public ResponseEntity<ArrayList<Product>> getAllProducts() {
+    ArrayList<Product> allProducts = productService.getAllProducts();
+    return new ResponseEntity<>(allProducts, HttpStatus.OK);
+  }
+
+  // READ (id)
+  @GetMapping("/{id}")
+  public ResponseEntity<Product> getProduct(@PathVariable long id) {
+    Product foundProduct = productService.getProduct(id);
+    return new ResponseEntity<>(foundProduct, HttpStatus.OK);
+  }
+
+  // READ (search by name)
+  @GetMapping("/search")
+  public ResponseEntity<ArrayList<Product>> searchProduct(@RequestParam String name) {
+    ArrayList<Product> products = productService.searchProduct(name);
+    return new ResponseEntity<>(products, HttpStatus.OK);
+  }
+
+  // READ (search by category)
+  @GetMapping("/search/category")
+  public ResponseEntity<ArrayList<Product>> searchCategory(@RequestParam String category) {
+    ArrayList<Product> foundCategory = productService.searchProductCat(category);
+    return new ResponseEntity<>(foundCategory, HttpStatus.OK);
+  }
+
+  // UPDATE
+  @PutMapping("/{id}")
+  public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+    // try {
+    Product updateProduct = productService.updateProduct(id, product);
+    return new ResponseEntity<>(updateProduct, HttpStatus.OK);
+    // } catch (ProductNotFoundException ex) {
+    // return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    // } -> Not needed already throw error in serviceImpls
+  }
+
+  // Delete - refactor try catch block by adding throw in ProductServiceImpl
+  @DeleteMapping("/{id}")
+  public ResponseEntity<HttpStatus> deleteProduct(@PathVariable long id) {
+    productService.deleteProduct(id);
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+  }
+
 }
--- src/main/java/sg/com/smartinventory/serviceImpls/ProductServiceImpl.java
+++ src/main/java/sg/com/smartinventory/serviceImpls/ProductServiceImpl.java
@@ -12,55 +12,72 @@ import sg.com.smartinventory.services.ProductService;
 
 @Service
 public class ProductServiceImpl implements ProductService {
-    private ProductRepository productRepository;
+  private ProductRepository productRepository;
 
-    // @Autowired
-    public ProductServiceImpl(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
+  // @Autowired
+  public ProductServiceImpl(ProductRepository productRepository) {
+    this.productRepository = productRepository;
+  }
 
-    @Override
-    public Product createProduct(Product product) {
-        Product newProduct = productRepository.save(product);
+  // - - - POST METHODS
+  @Override
+  public Product createProduct(Product product) {
+    Product newProduct = productRepository.save(product);
 
-        return newProduct;
-    }
+    return newProduct;
+  }
 
-    @Override
-    public Product getProduct(Long id) {
-        // Optional<Product> optionalProduct = productRepository.findById(id);
-        // if(optionalProduct.isPresent()) {
-        // Product foundProduct = optionalProduct.get();
-        // return foundProduct;
-        // }
-        // throw new ProductNotFoundException(id);
-        return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
-    }
+  // - - - GET METHODS
+  @Override
+  public Product getProduct(Long id) {
+    // Optional<Product> optionalProduct = productRepository.findById(id);
+    // if(optionalProduct.isPresent()) {
+    // Product foundProduct = optionalProduct.get();
+    // return foundProduct;
+    // }
+    // throw new ProductNotFoundException(id);
+    return productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+  }
 
-    @Override
-    public ArrayList<Product> getAllProducts() {
-        List<Product> allProducts = productRepository.findAll();
-        return (ArrayList<Product>) allProducts;
-    }
+  @Override
+  public ArrayList<Product> getAllProducts() {
+    List<Product> allProducts = productRepository.findAll();
+    return (ArrayList<Product>) allProducts;
+  }
 
-    @Override
-    public Product updateProduct(Long id, Product product) {
-        // Retrieve the product from the database.
-        Product productToUpdate = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+  @Override
+  public ArrayList<Product> searchProduct(String name) {
+    List<Product> products = productRepository.findByName(name);
+    return (ArrayList<Product>) products;
+  }
 
-        // Update the product retrieved from the database.
-        productToUpdate.setCategory(product.getCategory());
-        productToUpdate.setName(product.getName());
-        productToUpdate.setDescription(product.getDescription());
-        productToUpdate.setPrice(product.getPrice());
-        productToUpdate.setStockQuantity(product.getStockQuantity());
+  @Override
+  public ArrayList<Product> searchProductCat(String category) {
+    List<Product> products = productRepository.findByCategoryContaining(category);
+    return (ArrayList<Product>) products;
+  }
 
-        // Save the updated product back to the database.
-        return productRepository.save(productToUpdate);
-    }
+  // - - - PUT METHODS
+  @Override
+  public Product updateProduct(Long id, Product product) {
+    // Retrieve the product from the database.
+    Product productToUpdate = productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
 
-    @Override
-    public void deleteProduct(long id) {
-        productRepository.deleteById(id);
-    }
+    // Update the product retrieved from the database.
+    productToUpdate.setCategory(product.getCategory());
+    productToUpdate.setName(product.getName());
+    productToUpdate.setDescription(product.getDescription());
+    productToUpdate.setPrice(product.getPrice());
+    productToUpdate.setStockQuantity(product.getStockQuantity());
+
+    // Save the updated product back to the database.
+    return productRepository.save(productToUpdate);
+  }
+
+  // - - - DELETE METHODS
+  @Override // use long primitive type as args cannot be null
+  public void deleteProduct(long id) {
+    productRepository.findById(id).orElseThrow(() -> new ProductNotFoundException(id));
+    productRepository.deleteById(id);
+  }
 }
